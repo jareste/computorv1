ff12da0c031c949aa0ef7de8af66bded5a105e92
Joan Areste Gonzalez <jareste2000@gmail.com>
2024-01-26 16:17:23 +0100

    first parser part done

--- computor.py
+++ computor.py
@@ -1,22 +1,24 @@
-# import sys
-# from srcs.Parser import Parser
+import sys
+from srcs.parser import Parser
 from sympy import symbols, Eq, solve
 
 if __name__ == '__main__':
 
 	# i'll start parsing the arguments and format them in the (args, 0) format converting each ^ to **.
 
-	x = symbols('x')
-	eq1 = Eq(x**2 -x - 6, 0)
+	# x = symbols('x')
+	# eq1 = Eq(x**2 -x - 6, 0)
 
 
-	sol = solve(eq1)
+	# sol = solve(eq1)
 
-	print(sol)
-	# if len(sys.argv) != 2:
-	# 	print('Please introduce only 1 argument with correct format.')
-	# 	print('Format:')
-	# 	print('\"Equation to be solved in double quotes\"')
-	# 	exit(1)
-	# print(type(sys.argv[1]))
-	# Solver.solve(sys.argv[1])
+	# print(sol)
+	if len(sys.argv) != 2:
+		print('Please introduce only 1 argument with correct format.')
+		print('Format:')
+		print('\"Equation to be solved in double quotes\"')
+		exit(1)
+	try:
+		Parser.parse(sys.argv[1])
+	except Exception as e:
+		print(e)
--- srcs/parser.py
+++ srcs/parser.py
@@ -1,5 +1,5 @@
 from .tokenizer import Tokenizer
-
+import re
 
 class ParserError(Exception):
     pass
@@ -7,12 +7,29 @@ class ParserError(Exception):
 class Parser():
 	@staticmethod
 	def parse(av):
-		validChars = ['0','1','2','3','4','5','6','7','8','9','x','X','=','^','+','-','*','/','.',' ']
-		tokens = Tokenizer()
-		for char in av:
-			if char not in validChars:
-				raise ParserError("noooooooo")
-			else:
-				tokens.addToken(char)
-		tokens.expand()
+		if not re.match("^[0123456789xX=^+\\-*/. ]*$", av): 		# check for invalid characters
+			raise ParserError("Found an invalid character in the equation.")
+		rmeq = av.split('=')
+		if len(rmeq) != 2: # check for more than one equal sign
+			raise ParserError("Found more than one equal sign.")
+		rmeq[0] = rmeq[0].replace(' ', '')
+		rmeq[1] = rmeq[1].replace(' ', '')
+		if rmeq[0] == '' or rmeq[1] == '':	# check for empty sides
+			raise ParserError("Found an empty side.")
+		if re.search("[*/]{2,}", rmeq[0]) or re.search("[*/]{2,}", rmeq[1]): # check for more than one operator in a row
+			raise ParserError("Found more than one operator in a row.")
+		if len(rmeq[0]) == 1 and re.search("[+\-.\^*/]", rmeq[0]): # check for x = 0	
+			raise ParserError("Found an invalid equation.")
+		if len(rmeq[1]) == 1 and re.search("[+\-.\^*/]", rmeq[1]): # check for x = 0	
+			raise ParserError("Found an invalid equation.")
+		
+		print(rmeq)
+		
+		print('fino')
+		print("valid")
+		# validChars = set(['0','1','2','3','4','5','6','7','8','9','x','X','=','^','+','-','*','/','.',' '])
+		# tokens = Tokenizer()
+		# for char in av:
+		# 	if char not in validChars:
+		# 		raise ParserError("noooooooo")
 
