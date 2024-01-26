4d9222c43072b243eac0b17a3e233c201e1e0bf8
Joan Areste Gonzalez <jareste2000@gmail.com>
2024-01-26 15:49:31 +0100

    starting with a new view

--- computor.py
+++ computor.py
@@ -1,11 +1,22 @@
-import sys
-from srcs.solver import Solver
+# import sys
+# from srcs.Parser import Parser
+from sympy import symbols, Eq, solve
 
 if __name__ == '__main__':
-	if len(sys.argv) != 2:
-		print('Please introduce only 1 argument with correct format.')
-		print('Format:')
-		print('\"Equation to be solved in double quotes\"')
-		exit(1)
-	print(type(sys.argv[1]))
-	Solver.solve(sys.argv[1])
+
+	# i'll start parsing the arguments and format them in the (args, 0) format converting each ^ to **.
+
+	x = symbols('x')
+	eq1 = Eq(x**2 -x - 6, 0)
+
+
+	sol = solve(eq1)
+
+	print(sol)
+	# if len(sys.argv) != 2:
+	# 	print('Please introduce only 1 argument with correct format.')
+	# 	print('Format:')
+	# 	print('\"Equation to be solved in double quotes\"')
+	# 	exit(1)
+	# print(type(sys.argv[1]))
+	# Solver.solve(sys.argv[1])
--- srcs/solver.py
+++
@@ -1,9 +0,0 @@
-from .parser import Parser
-
-class Solver():
-	@staticmethod
-	def solve(av):
-		try:
-			Parser.parse(av)
-		except Exception as e:
-			print(f"An exception of type {type(e).__name__} occurred: {e}")
